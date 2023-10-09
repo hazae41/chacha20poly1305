e2a00c4d77462f17e92d4fe01ef27b11fb9fa397
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-10-10 00:57:32 +0200

    fix

--- src/mods/chacha20poly1305/noble.ts
+++ src/mods/chacha20poly1305/noble.ts
@@ -9,30 +9,28 @@ export function fromNoble(): Adapter {
   class Cipher {
 
     constructor(
-      readonly key: Box<Copiable<Uint8Array & { length: 32 }>>
+      readonly key: Uint8Array
     ) { }
 
-    [Symbol.dispose]() {
-      this.key[Symbol.dispose]()
-    }
+    [Symbol.dispose]() { }
 
-    static new(key: Box<Copiable<Uint8Array & { length: 32 }>>) {
+    static new(key: Uint8Array) {
       return new Cipher(key)
     }
 
     static tryImport(key: Box<Copiable<Uint8Array & { length: 32 }>>) {
-      return new Ok(new Cipher(new Box(new Copied(key.unwrap().copyAndDispose().bytes.slice() as Uint8Array & { length: 32 }))))
+      return new Ok(new Cipher(key.get().bytes.slice() as Uint8Array & { length: 32 }))
     }
 
     tryEncrypt(message: Box<Copiable>, nonce: Box<Copiable<Uint8Array & { length: 12 }>>) {
       return Result.runAndWrapSync(() => {
-        return chacha20poly1305(this.key.get().bytes, nonce.get().bytes.slice()).encrypt(message.get().bytes)
+        return chacha20poly1305(this.key, nonce.get().bytes.slice()).encrypt(message.get().bytes)
       }).mapErrSync(EncryptError.from).mapSync(Copied.new)
     }
 
     tryDecrypt(message: Box<Copiable>, nonce: Box<Copiable<Uint8Array & { length: 12 }>>) {
       return Result.runAndWrapSync(() => {
-        return chacha20poly1305(this.key.get().bytes, nonce.get().bytes.slice()).decrypt(message.get().bytes)
+        return chacha20poly1305(this.key, nonce.get().bytes.slice()).decrypt(message.get().bytes)
       }).mapErrSync(DecryptError.from).mapSync(Copied.new)
     }
 
