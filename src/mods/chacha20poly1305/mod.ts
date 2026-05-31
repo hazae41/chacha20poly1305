9daaa3c6961cde51d3b66ea7f8cbf79494262e5a
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-05-31 19:15:06 +0700

    r

--- src/mods/chacha20poly1305/mod.ts
+++ src/mods/chacha20poly1305/mod.ts
@@ -4,10 +4,19 @@ await chaCha20Poly1305Wasm.load()
 
 export class Cipher {
 
+  /**
+   * Do not use
+   * @param inner 
+   */
   constructor(
     readonly inner: chaCha20Poly1305Wasm.ChaCha20Poly1305Cipher
   ) { }
 
+  /**
+   * Import a key
+   * @param key 
+   * @returns 
+   */
   static import(key: Uint8Array) {
     const { Memory, ChaCha20Poly1305Cipher } = chaCha20Poly1305Wasm
 
@@ -16,6 +25,12 @@ export class Cipher {
     return new Cipher(inner)
   }
 
+  /**
+   * Encrypt a message with a nonce
+   * @param message 
+   * @param nonce 
+   * @returns 
+   */
   encrypt(message: Uint8Array, nonce: Uint8Array): Uint8Array {
     const { Memory } = chaCha20Poly1305Wasm
 
@@ -24,6 +39,12 @@ export class Cipher {
     return result.bytes
   }
 
+  /**
+   * Decrypt a message with a nonce
+   * @param message 
+   * @param nonce 
+   * @returns 
+   */
   decrypt(message: Uint8Array, nonce: Uint8Array): Uint8Array {
     const { Memory } = chaCha20Poly1305Wasm
 
