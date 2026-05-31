9ac0f2e211551471a290de8fac5ad3dacff65e4d
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-05-31 18:32:53 +0700

    f

--- src/mod.test.ts
+++ src/mod.test.ts
@@ -2,16 +2,14 @@ import { assert, test } from "@hazae41/phobos";
 import { chaCha20Poly1305 } from "./mod.ts";
 
 test("chacha", () => {
-  const { Memory, ChaCha20Poly1305Cipher } = chaCha20Poly1305.get().getOrThrow()
+  const key = crypto.getRandomValues(new Uint8Array(32))
+  const cipher = new chaCha20Poly1305.Cipher(key)
 
-  const key = Memory.fromOrThrow(crypto.getRandomValues(new Uint8Array(32)))
-  const cipher = ChaCha20Poly1305Cipher.importOrThrow(key)
+  const message = crypto.getRandomValues(new Uint8Array(256))
+  const nonce = crypto.getRandomValues(new Uint8Array(12))
 
-  const message = Memory.fromOrThrow(crypto.getRandomValues(new Uint8Array(256)))
-  const nonce = Memory.fromOrThrow(crypto.getRandomValues(new Uint8Array(12)))
+  const encrypted = cipher.encrypt(message, nonce)
+  const decrypted = cipher.decrypt(encrypted, nonce)
 
-  const encrypted = cipher.encryptOrThrow(message, nonce)
-  const decrypted = cipher.decryptOrThrow(encrypted, nonce)
-
-  assert(message.bytes.toString() === decrypted.bytes.toString())
+  assert(message.toString() === decrypted.toString())
 })
--- src/mods/chacha20poly1305/mod.ts
+++ src/mods/chacha20poly1305/mod.ts
@@ -1,4 +1,33 @@
-export * from "./abstract/mod.ts";
-export * from "./adapter/mod.ts";
-export * from "./wasm/mod.ts";
+import { chaCha20Poly1305Wasm } from "@hazae41/chacha20poly1305-wasm";
 
+await chaCha20Poly1305Wasm.load()
+
+export class Cipher {
+
+  #inner: chaCha20Poly1305Wasm.ChaCha20Poly1305Cipher
+
+  constructor(key: Uint8Array) {
+    const { Memory, ChaCha20Poly1305Cipher } = chaCha20Poly1305Wasm
+
+    this.#inner = new ChaCha20Poly1305Cipher(new Memory(key))
+
+    return
+  }
+
+  encrypt(message: Uint8Array, nonce: Uint8Array): Uint8Array {
+    const { Memory } = chaCha20Poly1305Wasm
+
+    const result = this.#inner.encrypt(new Memory(message), new Memory(nonce))
+
+    return result.bytes
+  }
+
+  decrypt(message: Uint8Array, nonce: Uint8Array): Uint8Array {
+    const { Memory } = chaCha20Poly1305Wasm
+
+    const result = this.#inner.decrypt(new Memory(message), new Memory(nonce))
+
+    return result.bytes
+  }
+
+}
